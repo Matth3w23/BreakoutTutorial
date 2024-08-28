86eb8029289356506a61ded6e195c296cecebc91
Matthew Hale <Matth3w4000@gmail.com>
2024-08-28 14:34:01 +0100

    Semicolon and updated includes

--- src/Game.h
+++ src/Game.h
@@ -1,6 +1,9 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 //represents the current state of the game
 enum GameState {
 	GAME_ACTIVE,
@@ -22,7 +25,7 @@ public:
 	//game loop
 	void ProcessInput(float dt);
 	void Update(float dt);
-	void Render()
+	void Render();
 };
 
 #endif
